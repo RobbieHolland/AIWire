4138b5bcc203d57fe2e9af85e469c0df5c0bacf4
Robert <Robert@Roberts-MacBook-Pro-2.local>
2019-11-17 14:30:44 +0000

    Random changes.

--- wire.m
+++ wire.m
@@ -1,43 +1,55 @@
-% Load image (to determine dimensions)
+%% Load image (to determine dimensions)
+rng(1234)
 im = load('../HASTE_GW_SAG_TR1200_S80_0012/HASTE_GW_SAG_TR1200_S80_0012.mat');
 im = im.imageDicom.image;
 
-% Simulated image
+%% Simulated image
 simulated = zeros(size(im(:,:,1)));
 size_f = size(simulated);
 
 % Define spline points
 point_spacing = 40;
 x = 1:point_spacing:size_f(1);
-y = randi([-30, 30], size(x)); 
+y = randi([-40, 40], size(x)); 
 
 % Interpolate and draw onto simulated image
 xx = 1:0.2:size_f(1);
 yy = spline(x,y,xx);
 
 yy = yy + size_f(2) / 2;
-xx = round(xx);
-yy = round(yy);
-for i = 1:size(xx, 2)
-    simulated(xx(i), yy(i)) = 1;
-end
 
-% Apply 1/r filter
-filter_dim = 30;
+g_yy = abs(atan(gradient(yy)));
+g_yy = max(g_yy) - g_yy;
+s = size(g_yy);
+
+simulated = drawFunc(simulated, xx, yy, ones(size(yy)));
+gradient_map = drawFunc(zeros(size(im(:,:,1))), xx, yy, g_yy);
+figure
+imshow(gradient_map, [])
+
+
+%% Apply 1/r filter
+filter_dim = 50;
 center = [filter_dim/2, filter_dim/2];
 [col_index, row_index] = ndgrid(1:filter_dim, 1:filter_dim);
 filter = sqrt(1 ./ ((row_index - center(1)).^2 + (col_index - center(2)).^2));
 filter(center(1), center(2)) = 1;
 
-simulated_conved = conv2(simulated, filter, 'same');
+gradient_map_conved = conv2(gradient_map, fspecial('gaussian'), 'same');
 
 % TODO: Add noise
 
 % Show result of filtering + noise
 figure
 subplot(1,2,1)
-imshow(simulated, [])
+imshow(gradient_map, [])
 title('Wire spline')
 subplot(1,2,2)
-imshow(simulated_conved,[])
+imshow(gradient_map_conved,[])
 title('Convolved with 1/r')
+
+% Left->Right gradient
+filter = fspecial('prewitt');
+grads = conv2(simulated, filter, 'same');
+figure
+imshow(grads,[])
