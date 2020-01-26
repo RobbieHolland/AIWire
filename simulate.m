1201c550fb814f7bf615ff74b48c630bcb666c46
Robert <Robert@Roberts-MacBook-Pro-2.local>
2020-01-26 16:50:02 +0000

    [RH] Generate dataset and add code to load into Notebook. Refactor directories.

--- simulate.m
+++ simulate.m
@@ -1,6 +1,9 @@
 function [thick_ground_truth, undersampled_noised_gradient_map] = simulate(blur, thickness, verbose)
-    im = zeros(256, 192, 203);
-    im_size = size(im(:,:,1));
+    addpath('util/')
+
+%     im = zeros(256, 192, 203);
+%     im_size = size(im(:,:,1));
+    im_size = [192 128];
 
     % Define spline
     [pts, spline_f] = gen_spline_realistic(im_size, 100);
@@ -9,7 +12,7 @@ function [thick_ground_truth, undersampled_noised_gradient_map] = simulate(blur,
        fnplt(spline_f) 
     end
     grads = gradient(pts);
-    
+
     % Normalise gradient vectors
     for i = 1:size(grads, 2)
         grads(:,i) = grads(:,i) / sqrt(sum(grads(:,i).^2));
@@ -20,7 +23,7 @@ function [thick_ground_truth, undersampled_noised_gradient_map] = simulate(blur,
     gradient_map = drawFunc(zeros(im_size), pts(2,:), pts(3,:), z_grads);
     
     % Apply 1/r filter
-    filter_dim = 500;
+    filter_dim = 250;
     center = [filter_dim/2, filter_dim/2];
     [col_index, row_index] = ndgrid(1:filter_dim, 1:filter_dim);
     filter_r = sqrt((1 ./ ((row_index - center(1)).^2 + (col_index - center(2)).^2)).^blur);
@@ -33,16 +36,15 @@ function [thick_ground_truth, undersampled_noised_gradient_map] = simulate(blur,
     sigma = 0.003;
     complex_guassian = normrnd(0, sigma, im_size) + 1i * normrnd(0, sigma, im_size);
     noised_gradient_map = abs(gradient_map_conved + complex_guassian);
-    
+
     % K-space artefacts
     k_space = itok(noised_gradient_map);
-    size(k_space)
     U = sampling_VD(flip(size(k_space)), 3)';
     undersampled_noised_gradient_map = abs(ktoi(U .* k_space));
-    
+
     % Ground-truth line thickness
     thick_ground_truth = conv2(ground_truth, ones(thickness), "same")>0;
-    
+
     if verbose
        % Show result of filtering + noise
         figure
