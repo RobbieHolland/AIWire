b91b76bfb412d8bec7892be404e4a794759c8665
RobbieHolland <robbie.holland3@gmail.com>
2020-02-10 19:00:21 +0000

    [RH] Add some visualisation and plotting. Fix bug in load_data.

--- load_data.py
+++ load_data.py
@@ -2,7 +2,7 @@ import scipy.io
 import os
 
 def load_data(dataset_path, dataset_name):
-    dataset = scipy.io.loadmat(os.path.join(dataset_path, dataset_name) + '.mat')[dataset_name]
+    dataset = scipy.io.loadmat(os.path.join(dataset_path, dataset_name) + '.mat')['dataset']
 
     X = dataset[0]
     y = dataset[1]
