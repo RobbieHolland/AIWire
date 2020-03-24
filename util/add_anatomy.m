9f1c51e2a80e0c3277dd433bb3250eb5d736c4c8
Ambra <ambra269@live.cn>
2020-03-24 20:21:53 +0000

    Change shape in anatomy

--- util/add_anatomy.m
+++ util/add_anatomy.m
@@ -1,19 +1,22 @@
-function [corrupted_image] = add_anatomy(image, im_size)%128, 64
+function [corrupted_image] = add_anatomy(image, im_size, opacity)%128, 64
+    if nargin == 2   % if the number of inputs equals 2
+        opacity = 1; % then make the third value, z, equal to my default value, 5.
+    end
     % Add anatomy to image here
     imshow(image)
     %Create coils on top of figure
-    left = annotation('rectangle',...
+    right = annotation('ellipse',...
+    [0.526490196078432 0.532455315145813 0.0623986928104575 0.230479774223893],...
+    'Color',[0.90,0.90,0.90],'FaceColor',[0.90*opacity,0.90*opacity,0.90*opacity]);
+    left = annotation('ellipse',...
     [0.413906910336949 0.547156227501801 0.0452592804111935 0.158387329013679],...
-    'Color',[0.65,0.65,0.65]); % smaller, darker, thinner 
-    right = annotation('rectangle',...
-    [0.52751528627015 0.525827814569536 0.0494719288493608 0.232450331125828],...
-    'Color',[0.90,0.90,0.90],'LineWidth',2);
+    'Color',[0.65,0.65,0.65],'LineWidth',2,'FaceColor',[0.65*opacity,0.65*opacity,0.65*opacity]);
+
     %save figure as stuct movie frame
     F = getframe;
     RGB = frame2im(F); %convert to image data(uint8)
     corrupted_image = im2double(RGB(:,:,1));%convert to double
-    
-    
+     
     % blur side patches
     leftthird = corrupted_image(:,1:ceil(im_size(2)/3)); 
     rightthird = corrupted_image(:,42: im_size(2));
