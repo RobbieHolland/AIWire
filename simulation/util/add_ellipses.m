2f47beeb5108f9e7015f5169492d0111937da61e
Ambra <ambra269@live.cn>
2020-05-03 00:23:33 +0100

    anatomy: random ellipses

--- simulation/util/add_ellipses.m
+++ simulation/util/add_ellipses.m
@@ -1,12 +1,28 @@
 function [corrupted_image] = add_ellipses(mask)%128, 64
+imshow(mask)
+set(gca,'position',[0 0 1 1],'units','normalized')
 
+%Generate random position, shape and size of ellipses
+leftcenter = 0.35+rand(1,1)*(0.5-0.35);
+leftx = 0.1+rand(1,1)*(0.4-0.1);%lower left
+lefty = 0.1+rand(1,1)*(0.4-0.1);
+leftw = 0+rand(1,1)*(leftcenter-leftx); %towards the middle
+lefth = lefty+rand(1,1)*(1-lefty);
+
+rightx = 0.65+rand(1,1)*(0.75-0.65); 
+righty = 0+rand(1,1)*(0.4-0);
+rightw = 0+rand(1,1)*(1-rightx);
+righth = righty+rand(1,1)*(1-righty);
+
+% set range for rand,avoid annotation, check deform ellipses call,
 right = annotation('ellipse',...
-    [0.526490196078432 0.532455315145813 0.0623986928104575 0.230479774223893],...
+    [rightx righty rightw righth],...
     'Color',[1,1,1],'FaceColor',[1,1,1]);
 
 left = annotation('ellipse',...
-    [0.413906910336949 0.547156227501801 0.0452592804111935 0.158387329013679],...
+    [leftx lefty leftw lefth],...
     'Color',[1,1,1],'LineWidth',2,'FaceColor',[1,1,1]);
+
 %save figure as stuct movie frame
 F = getframe; %getframe changed the image size
 RGB = frame2im(F); %convert to image data(uint8)
