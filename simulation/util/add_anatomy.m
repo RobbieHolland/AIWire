8edd899edd057d58a49e7845b88a128cbdd703a7
Ambra <ambra269@live.cn>
2020-05-08 21:01:58 +0100

    Anatomy: random noises inside

--- simulation/util/add_anatomy.m
+++ simulation/util/add_anatomy.m
@@ -5,10 +5,20 @@ function [corrupted_image] = add_anatomy(image,ratio, sizeanatomy)%128, 64
 %sizeanatomy [0,1]: at 1 the anatomy might occupy whole image, at 0 anatomy
 %is absent
 
+%Add ellipses on black mask
 mask = zeros(size(image));
 corrupted_image = add_ellipses(mask,ratio, sizeanatomy);
 
+%Fill in with random noise
+for i = 1:size(mask,1)
+    for j= 1:size(mask,2)
+        if corrupted_image(i,j) == 1
+            corrupted_image(i,j) = rand;
+        end
+    end
+end
+
+%Add elastic transformation to ellipses
 deformed = deform_ellipses(corrupted_image);
-%new = imgaussfilt(deformed, 1,'FilterSize',7);  
 corrupted_image = deformed+image;            
 end
