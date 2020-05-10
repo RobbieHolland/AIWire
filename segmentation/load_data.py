67eb61a260e44b1b3d9ff06606891aeec5767360
RobbieHolland <robbie.holland3@gmail.com>
2020-05-10 18:06:31 +0100

    [RH] Enable loading large .mat files.

--- segmentation/load_data.py
+++ segmentation/load_data.py
@@ -1,14 +1,18 @@
 import scipy.io
 import os
+import h5py 
+import numpy as np
 
-def load_data(dataset_path, dataset_name, spline_pts=False):
-    data = scipy.io.loadmat(os.path.join(dataset_path, dataset_name) + '.mat')
-    image_data = data['dataset']
+def load_data(dataset_path, dataset_name, v73=False):
+    path = os.path.join(dataset_path, dataset_name) + '.mat'
+    if v73:
+        with h5py.File(path, 'r') as f: 
+            data = {'dataset': np.moveaxis(np.array(f['dataset']), (0, 1, 2, 3), (3, 2, 1, 0)), 'spline_pts': np.array(f['spline_pts'])}
+    else:
+        data = scipy.io.loadmat(path)
 
-    X = image_data[0]
-    y = image_data[1]
-
-    if spline_pts:
-        return X, y, data['spline_pts']
+    X = data['dataset'][0]
+    y = data['dataset'][1]
+    spline_pts = data['spline_pts']
 
     return X, y
