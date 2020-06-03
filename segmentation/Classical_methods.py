a2a4e7e9962874aed41f6db687de936449ab2e2c
Robert <Robert@Roberts-MBP-2.broadband>
2020-06-03 15:11:44 +0100

    [RH] Guassian before laplace filter.

--- segmentation/Classical_methods.py
+++ segmentation/Classical_methods.py
@@ -1,9 +1,17 @@
 import cv2
 import numpy as np 
+from scipy.ndimage import gaussian_filter
 
-def laplace_segmentation(image, threshold = 0.52):
+def laplace_segmentation(image, sigma = 7, threshold = 0.5):
+    image = gaussian_filter(image, sigma=sigma)
     lp = -cv2.Laplacian(image, cv2.CV_64F, ksize=15)
     lp = lp - np.min(lp)
     lp = lp / np.max(lp)
+#     return image, lp# > threshold
     return lp > threshold
 
+def laplace_segmentation_old(image, threshold = 0.52):
+    lp = -cv2.Laplacian(image, cv2.CV_64F, ksize=150)
+    lp = lp - np.min(lp)
+    lp = lp / np.max(lp)
+    return lp > threshold
