71a48aea3daa8e597906b7233420e5eae58adf4f
Robert <Robert@Roberts-MBP-2.broadband>
2020-06-04 11:46:49 +0100

    [RH] Adaptive threshold laplace filter.

--- segmentation/Classical_methods.py
+++ segmentation/Classical_methods.py
@@ -2,6 +2,16 @@ import cv2
 import numpy as np 
 from scipy.ndimage import gaussian_filter
 
+def laplace_segmentation_adaptive(image, sigma = 7, adaptive_threshold = 0.85):
+    image = gaussian_filter(image, sigma=sigma)
+    lp = -cv2.Laplacian(image, cv2.CV_64F, ksize=15)
+    lp = lp - np.min(lp)
+    lp = lp / np.max(lp)
+    intensities = np.sort(lp.flatten())
+#     return image, lp# > threshold
+    threshold = intensities[round(intensities.shape[0] * adaptive_threshold)]
+    return lp > threshold
+
 def laplace_segmentation(image, sigma = 7, threshold = 0.5):
     image = gaussian_filter(image, sigma=sigma)
     lp = -cv2.Laplacian(image, cv2.CV_64F, ksize=15)
